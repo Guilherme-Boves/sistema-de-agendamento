56eceffc88e348803493648bc7924d6bf151e33a
Guilherme Boves <ghboves@gmail.com>
2023-02-13 22:13:39 -0300

    Criação do service de cadastro de consultas

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const appointmentService = require("./services/AppointmentService");
 
 app.use(express.static("public"));
 
@@ -18,6 +19,63 @@ app.get("/", (req, res) => {
 
 app.get("/cadastro", (req, res) => {
     res.render("create.ejs");
+})
+
+app.post("/create", async (req, res) => {
+
+    var {name, email, description, cpf, date, time} = req.body;
+
+    if(name == undefined || name == ''){
+        console.log("Nome inválido");
+        res.status(400);
+        return;
+    }
+
+    if(email == undefined || email == ''){
+        console.log("E-mail inválido");
+        res.status(400);
+        return;
+    }
+
+    if(description == undefined || description == ''){
+        console.log("Descrição inválido");
+        res.status(400);
+        return;
+    }
+
+    if(cpf == undefined || cpf == ''){
+        console.log("CPF inválido");
+        res.status(400);
+        return;
+    }
+
+    if(date == undefined || date == ''){
+        console.log("Data inválida");
+        res.status(400);
+        return;
+    }
+
+    if(time == undefined || time == ''){
+        console.log("Horário inválido");
+        res.status(400);
+        return;
+    }
+
+    var status = await appointmentService.Create(
+        name,
+        email,
+        description,
+        cpf,
+        date,
+        time
+    )
+
+    if(status){
+        res.redirect("/")
+    } else {
+        res.status(400)
+        res.send("Ocorreu uma falha!")
+    }
 })
 
 app.listen(3000, () => {
